5f79c56ace6e1f38085b553e263c02910045bb7e
samuelgocnik <gocnik@webscope.io>
2024-04-09 00:29:15 +0200

    fix: home page error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
+const Home = () => <main className="">HomePage</main>;
 
-export default function Home() {
-	return <main className="">HomePage</main>;
-}
+export default Home;
