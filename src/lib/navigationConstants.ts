21b4d4765ceefcb18c8a8824d91426aee93c9d73
samuelgocnik <gocnik@webscope.io>
2024-05-01 16:19:22 +0200

    feat(nav): define nav links

---
+++ src/lib/navigationConstants.ts
@@ -0,0 +1,14 @@
+export const NAVIGATION_LINKS = [
+	{
+		title: 'Dashboard',
+		href: '/dashboard'
+	},
+	{
+		title: 'Groups',
+		href: '/group'
+	},
+	{
+		title: 'Expenses',
+		href: '/expenses'
+	}
+];
