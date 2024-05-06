7587011a72c1aa2e1b52962325a70d2ad0d271b7
Zyfix <45912417+Zyfix@users.noreply.github.com>
2024-05-06 03:19:22 +0200

    WIP db query getGroupsPreview does not work as expected, users join table is not incluced

--- src/queries/groups.ts
+++ src/queries/groups.ts
@@ -1,19 +1,43 @@
 'use server';
 
+import { desc } from 'drizzle-orm';
+
 import { db } from '@/db';
-import { type Groups } from '@/db/schema/groups';
+import { expenses } from '@/db/schema/expenses';
+import { type Groups, type GroupsWithRelations } from '@/db/schema/groups';
 
 export const getGroupsPreview = async (
 	userId: string,
 	limit?: number
 ): Promise<Groups[]> => {
-	/* TODO We want to show in the dashboard groups that are
-		- relevant (user is member of) groups
-		- recent transactions, or "most used groups" by the user
-		- most active (number of members)
-
-		- Add a filter to only show groups that the user is a member of
+	// TODO We want to show in the dashboard groups that are relevant (user is member of) groups and recent activities
+	/* Steps:
+		- Find all groups that the user is a member of
+		- join expenses and users tables
+		- from users table we want to find if the user is a member of the group
+		- from expenses table we want to find the most recent expense (based on createdAt) for each group
+		
+		- now the prepared groups, are sorted based on their most recent includedexpense
+		- return number of groups based on the limit
 	*/
+
+	// TODO the query does not include any records from the relations "users"
+	const tmp: GroupsWithRelations[] = await db.query.groups.findMany({
+		with: {
+			users: {
+				with: {
+					user: true
+				}
+			},
+			expenses: {
+				// get the most recent expense for each group
+				orderBy: desc(expenses.createdAt),
+				limit: 1
+			}
+		}
+	});
+	console.log('Groups that should included users and one expense:', tmp);
+
 	const groups: Groups[] = await db.query.groups.findMany({
 		limit
 	});
