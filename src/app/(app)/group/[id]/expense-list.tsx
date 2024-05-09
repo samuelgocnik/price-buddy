e6b5e3c0b063c576de5f796478dd979e3c9ffd72
Tomas Marek <485386@mail.muni.cz>
2024-05-09 20:58:08 +0200

    fix: some todos removed

--- src/app/(app)/group/[id]/expense-list.tsx
+++ src/app/(app)/group/[id]/expense-list.tsx
@@ -4,22 +4,16 @@ import { Trash2 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { getGroupsExpenses } from '@/queries/expenses';
 
-import { getUsers } from '../../../../queries/group';
-
 export const ExpenseList = async ({ id }: { id: string }) => {
 	const expenses = await getGroupsExpenses(id);
 
-	// TODO 3 - can be removed after resolving td 2
-	const users = await getUsers();
 	return (
 		<div>
 			{expenses.length === 0 && <p>No expenses</p>}
 			{expenses.map(expense => (
 				<div key={expense.id} className="my-2 grid grid-cols-4">
 					<p className="my-auto">{expense.title}</p>
-					<p className="my-auto text-center">
-						{users.filter(u => u.id === expense.paidById)[0].name}
-					</p>
+					<p className="my-auto text-center">{expense.paidBy.name}</p>
 					<p className="my-auto mr-2 text-right">{expense.amount} €</p>
 					<Button variant="destructive" className="my-auto ml-auto h-8 w-16">
 						<Trash2 size={15} />
--- src/queries/expenses.ts
+++ src/queries/expenses.ts
@@ -7,8 +7,8 @@ import { expenses } from '@/db/schema/expenses';
 
 export const getGroupsExpenses = async (id: string) =>
 	await db.query.expenses.findMany({
-		where: eq(expenses.groupId, id) //,
-		// with: {
-		// 	user: true // TODO 2
-		// }
+		where: eq(expenses.groupId, id),
+		with: {
+			paidBy: true
+		}
 	});
