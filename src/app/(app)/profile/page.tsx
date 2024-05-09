a7fb634f02298a408f72f7ffc1ef0d27205f4c7f
samuelgocnik <gocnik@webscope.io>
2024-05-09 16:20:59 +0200

    style: profile info

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -27,32 +27,32 @@ const ProfilePage = async () => {
 				/>
 			</div>
 			<div className="space-y-4">
-				<div className="rounded-lg bg-almond-100 p-4 md:flex md:flex-col md:justify-center">
+				<div className="rounded-lg bg-almond-100 p-4 max-md:space-y-2 md:flex md:flex-col md:justify-center">
 					{hasSetName ? (
 						<>
-							<div className="grid grid-cols-[1fr_3fr] gap-x-3">
-								<span className="text-right text-blue-smoke-600">
+							<div className="grid max-md:grid-rows-2 md:grid-cols-[1fr_3fr] md:gap-x-3">
+								<span className="text-blue-smoke-600 md:text-right">
 									First name:
 								</span>
 								<span className="font-medium">{user.firstName}</span>
 							</div>
-							<div className="grid grid-cols-[1fr_3fr] gap-x-3">
-								<span className="text-right text-blue-smoke-600">
+							<div className="grid max-md:grid-rows-2 md:grid-cols-[1fr_3fr] md:gap-x-3">
+								<span className="text-blue-smoke-600 md:text-right">
 									Last name:
 								</span>
 								<span className="font-medium">{user.lastName}</span>
 							</div>
 						</>
 					) : (
-						<div className="grid grid-cols-[1fr_3fr] gap-x-3">
-							<span className="text-right text-blue-smoke-600">
+						<div className="grid max-md:grid-rows-2 md:grid-cols-[1fr_3fr] md:gap-x-3">
+							<span className="text-blue-smoke-600 md:text-right">
 								Github name:
 							</span>
 							<span className="font-medium">{user.name}</span>
 						</div>
 					)}
-					<div className="grid grid-cols-[1fr_3fr] gap-x-3">
-						<span className="text-right text-blue-smoke-600">Email:</span>
+					<div className="grid max-md:grid-rows-2 md:grid-cols-[1fr_3fr] md:gap-x-3">
+						<span className="text-blue-smoke-600 md:text-right">Email:</span>
 						<span className="font-medium">{user.email}</span>
 					</div>
 				</div>
