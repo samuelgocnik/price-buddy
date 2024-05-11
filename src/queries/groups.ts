9ef7aa5beaf6e151e48338b9f71bbd474f6db640
Zyfix <45912417+Zyfix@users.noreply.github.com>
2024-05-11 02:02:51 +0200

    feat(groups): db query for the user related groups

--- src/queries/groups.ts
+++ src/queries/groups.ts
@@ -1,18 +1,24 @@
 'use server';
 
-import { desc } from 'drizzle-orm';
+import { desc, eq } from 'drizzle-orm';
 
 import { db } from '@/db';
 import { expenses } from '@/db/schema/expenses';
 import { type Groups, type GroupsWithRelations } from '@/db/schema/groups';
+import { usersGroups } from '@/db/schema/userGroups';
 
+// TODO fix after DB reset
 export const getGroupsPreview = async (
 	userId: string,
 	limit?: number
 ): Promise<Groups[]> => {
-	// TODO We want to show in the dashboard groups that are relevant (user is member of) groups and recent activities
+	// We want to show in the dashboard groups that are relevant (user is member of) groups and recent activities
 	/* Steps:
-		- Find all groups that the user is a member of
+		- Find all groups, and include users and expenses
+		- from users table we want to find if the user is a member of the group
+		- from expenses table we want to find the most recent expense (based on createdAt) for each group
+		- now the prepared groups, are sorted based on their most recent includedexpense
+		- return number of groups based on the limit
 		- join expenses and users tables
 		- from users table we want to find if the user is a member of the group
 		- from expenses table we want to find the most recent expense (based on createdAt) for each group
@@ -21,29 +27,49 @@ export const getGroupsPreview = async (
 		- return number of groups based on the limit
 	*/
 
-	// TODO the query does not include any records from the relations "users"
-	const tmp: GroupsWithRelations[] = await db.query.groups.findMany({
-		with: {
-			users: {
-				with: {
-					user: true
-				}
-			},
-			expenses: {
-				// get the most recent expense for each group
-				orderBy: desc(expenses.createdAt),
-				limit: 1
-			}
-		}
-	});
-	console.log('Groups that should included users and one expense:', tmp);
+	console.log('User id:', userId);
 
-	const groups: Groups[] = await db.query.groups.findMany({
-		limit
+	const groupsWithRelations: GroupsWithRelations[] =
+		await db.query.groups.findMany({
+			with: {
+				users: {
+					// Array will include only user with the provided userId
+					where: eq(usersGroups.userId, userId),
+					with: {
+						user: true
+					}
+				},
+				expenses: {
+					// get the most recent expense for each group
+					orderBy: desc(expenses.createdAt),
+					limit: 1
+				}
+			}
+		});
+
+	const groupsForUser = groupsWithRelations.filter(g => g.users.length === 1);
+
+	// sort based on the most recent expense
+	groupsForUser.sort((a, b) => {
+		const aDate = new Date(a.expenses[0]?.createdAt).getTime();
+		const bDate = new Date(b.expenses[0]?.createdAt).getTime();
+		return aDate - bDate;
 	});
+
+	// groupsForUser.forEach(group => {
+	// 	console.log("Group:", group.name, "\nExpenses:");
+	// 	console.log(group.expenses)
+	// });
+
+	// limit the number of groups based on the limit
+
+	console.log(
+		'Groups that should included users and one expense:',
+		groupsForUser
+	);
 
 	await sleep(2000);
-	return groups;
+	return groupsForUser;
 };
 
 const sleep = (time: number) =>
