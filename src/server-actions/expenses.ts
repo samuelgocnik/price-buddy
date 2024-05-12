3ceda84be6f7290a73b61caa833b473e1eaf2520
Diana Gulčíková <xgulcik@fi.muni.cz>
2024-05-12 23:29:21 +0200

    fix: users expenses are updated

--- src/server-actions/expenses.ts
+++ src/server-actions/expenses.ts
@@ -3,12 +3,17 @@ import { revalidatePath } from 'next/cache';
 
 import { type ExpensesCreate, expenses } from '@/db/schema/expenses';
 import { db } from '@/db';
+import { usersExpenses } from '@/db/schema/usersExpenses';
 
 import { updateBalancesAfterExpense } from './userBalances';
 
 export const addExpenseAction = async (expense: ExpensesCreate) => {
 	const expenseResult = await db.insert(expenses).values(expense).returning();
 	if (expenseResult.length > 0) {
+		await db.insert(usersExpenses).values({
+			userId: expense.paidById,
+			expenseId: expenseResult[0].id
+		});
 		await updateBalancesAfterExpense(
 			expense.paidById,
 			expense.groupId,
