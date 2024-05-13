34188eabdf611dcb55044f4fc1ec4a884e39ceeb
Tomas Marek <485386@mail.muni.cz>
2024-05-13 14:32:02 +0200

    fix: expenses

---
+++ src/app/(app)/expenses/loading.tsx
@@ -0,0 +1,9 @@
+import Loader from '@/components/ui/loader';
+
+const Loading = () => (
+	<div className="flex h-screen items-center justify-center">
+		<Loader size="2xl" className="text-gray-ý00" />
+	</div>
+);
+
+export default Loading;
--- src/app/(app)/expenses/page.tsx
+++ src/app/(app)/expenses/page.tsx
@@ -1,14 +1,23 @@
 import { Hourglass } from 'lucide-react';
+import { Suspense } from 'react';
 
 import { ExpenseList } from './expense-list';
 
 const ExpensesPage = () => (
-	<div className="mx-4 lg:mx-16">
+	<div>
 		<h1 className="flex flex-row rounded-t-lg bg-almond-200 py-4 pl-8 text-lg">
 			<Hourglass className="mr-6 mt-0.5" />
 			My recent expenses
 		</h1>
-		<ExpenseList />
+		<Suspense
+			fallback={
+				<div className="flex h-32 items-center justify-center rounded-b-lg bg-almond-100">
+					Loading...
+				</div>
+			}
+		>
+			<ExpenseList />
+		</Suspense>
 	</div>
 );
 
