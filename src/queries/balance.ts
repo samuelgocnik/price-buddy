6c4d730f898bdbc9c8cf395c20c125047a9cf363
Zyfix <45912417+Zyfix@users.noreply.github.com>
2024-05-14 01:34:35 +0200

    feat: added UserBalances queries

---
+++ src/queries/balance.ts
@@ -0,0 +1,45 @@
+import { and, eq, isNull } from 'drizzle-orm';
+
+import { db } from '@/db';
+import {
+	userBalances,
+	type BalancesWithUsers,
+	type UserBalances
+} from '@/db/schema/userBalances';
+import { safeAccAdd, sleep } from '@/lib/utils';
+
+export const getAllBalances = async (
+	userId: string
+): Promise<BalancesWithUsers[]> => {
+	const balances: BalancesWithUsers[] = await db.query.userBalances.findMany({
+		columns: {
+			id: true,
+			balance: true
+		},
+		where: and(
+			eq(userBalances.user1Id, userId),
+			isNull(userBalances.deletedAt)
+		),
+		with: {
+			user1: true,
+			user2: true
+		}
+	});
+
+	console.log('Balances:', balances);
+	await sleep(2000);
+	return balances;
+};
+
+export const getUserTotalBalance = async (userId: string): Promise<number> => {
+	const balances: UserBalances[] = await db.query.userBalances.findMany({
+		where: and(eq(userBalances.user1Id, userId), isNull(userBalances.deletedAt))
+	});
+
+	const totalBalance = balances.reduce(
+		(acc, balance) => safeAccAdd(acc, balance.balance),
+		0.0
+	);
+	await sleep(2000);
+	return totalBalance;
+};
