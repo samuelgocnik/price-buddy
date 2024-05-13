5faa06d31d11e73caf4abe017837ec5964dbd643
Tomas Marek <485386@mail.muni.cz>
2024-05-13 20:10:32 +0200

    del loading

--- src/app/(app)/expenses/loading.tsx
+++
@@ -1,9 +0,0 @@
-import Loader from '@/components/ui/loader';
-
-const Loading = () => (
-	<div className="flex h-screen items-center justify-center">
-		<Loader size="2xl" className="text-gray-ý00" />
-	</div>
-);
-
-export default Loading;
