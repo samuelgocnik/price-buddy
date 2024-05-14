9d8135fd97a37e0851bc8cde4e8a5802c3bcb314
samuelgocnik <gocnik@webscope.io>
2024-05-14 17:01:43 +0200

    feat: skeleton map

--- src/app/(app)/(group)/group/[id]/page.tsx
+++ src/app/(app)/(group)/group/[id]/page.tsx
@@ -2,7 +2,7 @@ import { type Metadata } from 'next';
 import { Suspense } from 'react';
 
 import { getGroupName } from '@/queries/groups';
-import { Skeleton } from '@/components/ui/skeleton';
+import { Skeleton, SkeletonMap } from '@/components/ui/skeleton';
 import { GroupName } from '@/components/groups/group-name';
 import { GroupInfo } from '@/components/groups/group-info';
 import { ExpenseList } from '@/components/groups/expense-list';
@@ -26,19 +26,19 @@ const GroupDetailPage = async ({ params }: GroupDetailPageProps) => (
 	<GroupDetail
 		id={params.id}
 		name={
-			<Suspense fallback={<Skeleton className="h-9 w-24 bg-blue-smoke-200" />}>
+			<Suspense fallback={<Skeleton className="h-9 w-48 bg-almond-300" />}>
 				<GroupName groupId={params.id} />
 			</Suspense>
 		}
 		expenseList={
-			<Suspense fallback={<Skeleton className="h-9 w-24 bg-blue-smoke-200" />}>
+			<Suspense
+				fallback={<SkeletonMap count={5} className="h-9 bg-almond-300" />}
+			>
 				<ExpenseList id={params.id} />
 			</Suspense>
 		}
 		groupInfo={
-			<Suspense
-				fallback={<Skeleton className="h-9 w-full bg-blue-smoke-200" />}
-			>
+			<Suspense fallback={<Skeleton className="h-24 bg-almond-300" />}>
 				<GroupInfo groupId={params.id} />
 			</Suspense>
 		}
